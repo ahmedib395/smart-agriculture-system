36923c50b29731aef1a830c16129ebdb2f829714
ahmedib395 <ahmedib5678@gmail.com>
2026-04-28 23:50:14 +0300

    Initialize backend server with WebSocket and MQTT
    Set up Express server with WebSocket and MQTT integration for smart agriculture.

---
+++ backend/server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const mqtt = require("mqtt");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const PORT = process.env.PORT || 3000;
+
+const mqttClient = mqtt.connect("mqtts://491651ab41c341269c0b6942b3a72aaf.s1.eu.hivemq.cloud:8883", {
+  username: "ahmedhassan",
+  password: "Sas51015",
+  rejectUnauthorized: false
+});
+
+const TOPIC_MOISTURE = "plantguard_x7k92mf/moisture";
+const TOPIC_PUMP = "plantguard_x7k92mf/pump";
+const TOPIC_STATUS = "plantguard_x7k92mf/status";
+
+app.get("/", (req, res) => {
+  res.send("Smart Agriculture Backend Server is running");
+});
+
+mqttClient.on("connect", () => {
+  console.log("Connected to HiveMQ Cloud");
+  mqttClient.subscribe(TOPIC_MOISTURE);
+  mqttClient.subscribe(TOPIC_STATUS);
+});
+
+mqttClient.on("message", (topic, message) => {
+  const text = message.toString();
+
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      if (topic === TOPIC_MOISTURE) {
+        client.send(JSON.stringify({
+          type: "moisture",
+          value: text
+        }));
+      }
+
+      if (topic === TOPIC_STATUS) {
+        client.send(JSON.stringify({
+          type: "status",
+          value: JSON.parse(text)
+        }));
+      }
+    }
+  });
+});
+
+wss.on("connection", (ws) => {
+  console.log("Web Dashboard connected");
+
+  ws.on("message", (msg) => {
+    const data = JSON.parse(msg.toString());
+
+    if (data.type === "pump_command") {
+      mqttClient.publish(TOPIC_PUMP, data.value);
+      console.log("Command sent to ESP32:", data.value);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server bridge running on port ${PORT}`);
+});
